faca41d7e89a74fba307c6bfd378b6ca91352c45
Kelvin Cao <kelvincaoyx@gmail.com>
2022-04-22 12:44:17 -0400

    2017 test

---
+++ finals/final_2017.py
@@ -0,0 +1,311 @@
+'''
+neutron++1
+is the best part
+'''
+# particles = ["neutron", "proton", "electron"]
+# charges = ["0", "+1", "-1"]
+# print(particles[0] + "+" + charges[1])
+# print("is the best particle"[:-4])
+
+'''
+we are\we are\we are
+THE ENGINEERS!!!
+'''
+
+# final = '!'
+# final *= 3
+# lyric = "we are\\"
+# print(lyric)
+# chorus = "the engineers".upper()
+# chorus.lower()
+
+# for i in range(2):
+#     print(i)
+#     lyric += lyric
+
+# print(lyric, chorus, sep = '\n', end = final)
+
+'''
+
+
+'''
+# import csv
+# with open('test.csv') as csvfile:
+#     file_reader = csv.reader(csvfile)
+#     for row in file_reader:
+#         print(row[0] + 'exam on' + '-'.join(row[1:-1]) + 'at'  
+#             + row[-1])
+
+'''
+1000
+100
+10
+1
+10
+100
+1000
+
+'''
+from itertools import count
+import re
+
+
+def fun1(n):
+    if n < 10:
+        print(n)    
+    else:
+        print(n)
+        fun1(n//10)
+        print(n)
+        
+# fun1(1000)
+
+'''
+{0:H, 2:l, 4:o}
+
+0->H
+N/A
+2->1
+N/a
+4->o
+N/A
+
+'''
+# message = "Hello!"
+# positions = [0,1,2,3,4,5]
+# my_dictionary = {}
+# count = 0 
+# for i in range(0, len(positions), 2):
+#     my_dictionary[positions[i]] = message[count]
+#     count += 2
+ 
+# for element in positions:
+#     print(element, end="-> ")
+#     if element in my_dictionary:
+#         print(my_dictionary[element])
+#     else:
+#         print("N/A")
+
+
+def sum_ascii(s):
+    ''' (str) -> int 
+    Output: Returns the sum of the ASCII values of all the characters 
+    in string s.
+    '''
+    counter = 0
+    for i in s:
+        counter += ord(i)
+    
+    return counter
+        
+# print(sum_ascii(""))
+
+def pi_over_4(epsilon):
+    """(float) -> (float, int)
+    Input: A number determining the accuracy of the estimate of pi/4.
+    Output: Returns a tuple containing an estimate of pi/4 and the 
+    number of terms summed to generate the estimate.
+    """
+    returnCounter = 0
+    for i in range(epsilon):
+        returnCounter += ((-1)**i)/(2*i +1)
+    return returnCounter, epsilon
+
+# print(pi_over_4(2))
+
+class ComplexNumber:
+    def __init__(self, real = 0, imaginary = 0):
+        ''' Creates a complex number with two data attributes: 
+        a real part (real) and an imaginary part (imaginary).
+        '''
+        self.real = real
+        self.imaginary = imaginary
+        
+    def __str__(self):
+        '''(ComplexNumber) -> str
+        Output: Returns a string representing the complex number.
+        The format of the string is '(r + si)' where r is the
+        real part and s is the imaginary part. 
+        >>> c = ComplexNumber(3.1,4.2)
+        >>> print(c)
+        (3.1 + 4.2i)
+        The parenthesis and i are part of the string.
+        '''
+        return "(" + str(self.real) + " + " + str(self.imaginary) + "i)"
+
+    def add(self, other):
+        '''(ComplexNumber, ComplexNumber) -> ComplexNumber
+        Output: Returns a new ComplexNumber object resulting from
+        adding the ComplexNumbers self and other. 
+        '''
+        new = ComplexNumber(self.real + other.real, self.imaginary + other.imaginary )
+        return new
+    
+# print(ComplexNumber(5,4).add(ComplexNumber(8,3)))
+
+class Node:
+    def __init__(self):
+        '''Initialize a Node'''
+        self.cargo = None
+        self.next = None
+        
+class Stack:
+    def __init__(self):
+        '''Initialize a Stack'''
+        self.length = 0 # the number of elements in the Stack
+        self.head = None
+    
+    def is_empty(self):
+        '''(Stack) -> bool
+        Output: Return True if the Stack is empty and False otherwise.
+        '''
+        if self.head == None:
+            return True
+        return False
+    
+    def peek(self):
+        '''(Stack) -> the type of the cargo or NoneType
+        Output: Return the cargo of the element at the front of the
+        Stack or None if the Stack is empty.
+        '''
+        if self.is_empty():
+            return None
+        
+        return self.head.cargo
+    
+    def pop(self):
+        '''(Stack) -> the type of the cargo or NoneType
+        Output: Remove the first element of the Stack and return the
+        cargo. Return None if the Stack is empty. Update self.length
+        '''
+        if self.is_empty():
+            return None
+        
+        current = self.head
+        self.head = self.head.next
+        
+        current.next = None
+        
+        self.length -= 1
+        
+        return current.cargo
+    
+    def __str__(self):
+        current = self.head
+        
+        while current != None:
+            print(current.cargo)
+            current = current.next
+    
+    def push(self, cargo):
+        '''(Stack, type of cargo) -> NoneType
+        Output: Insert a new item at the front of the Stack. Return
+        None. Update self.length
+        '''
+        new = Node()
+        new.cargo = cargo
+        new.next = self.head
+        self.head = new
+        self.length += 1
+
+    
+# one = Node()
+# two = Node()
+# three = Node()
+
+# one.cargo = 1
+# two.cargo = 2
+# three.cargo = 3
+
+# one.next = two
+# two.next = three
+
+# s = Stack()
+# s.length = 3
+# s.head = one
+
+# s.__str__()
+
+# print(s.pop())
+
+# print("###########")
+# s.push(69)
+
+# s.__str__()
+# print(s.length)
+
+import csv
+def load_inventory(filename):
+    '''(str) -> dictionary {str : [[int, str], [int, str], ...]]}
+    Input: a string specifying the CSV filename
+    Output: a dictionary representing the inventory, amounts, and
+    locations. 
+    ''' 
+    with open(filename) as csvFile:
+        
+        dictt = dict()
+        
+        listVersion = []
+        for row in csv.reader(csvFile):
+            listVersion.append(row)
+        
+        for row in listVersion:
+            key = row[0]
+            if key not in dictt:
+                dictt.update({key:[]})
+            dictt[key].append([row[1],row[2]])
+        
+        return dictt
+
+# print(load_inventory("inv.csv"))
+
+def is_correct(prediction, label):
+    '''(list, list) -> bool
+    Input: two lists, prediction and label, of floating point numbers
+    representing probability values corresponding to digits 0 – 9
+    Output: True only if most probable digit in prediction matches
+    label. False, otherwise.
+    '''
+    highIndex = 0
+    highNum = prediction[highIndex]
+    for i in range(len(prediction)):
+        if prediction[i] > highNum:
+            highIndex = i
+            highNum = prediction[i]
+    
+    highIn = 0
+    for i in range(len(label)):
+        if label[i] == 1:
+            highIn = i
+    
+    if highIn == highIndex:
+        return True
+    
+    return False
+
+predictions = [[0.06, 0.01, 0.02, 0.01, 0.5, 0.05, 0.01, 0.01, 0.08, 0.3],
+ [0.01, 0.02, 0.06, 0.7, 0.1, 0.07, 0.06, 0.01, 0.01, 0.01],
+ [0.35, 0.07, 0.01, 0.01, 0.05, 0.01, 0.45, 0.02, 0.06, 0.02]]
+labels = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+ [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+ [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+
+def percent_correct(predictions, labels):
+    '''(list of lists, list of lists) -> float
+    Input: two nested lists of floating point numbers
+    Output: a float representing a percentage score of accurately 
+    classified digit samples over the total number of samples.
+    Pre-conditions: all sub-lists have the same size and contain
+    only floating point numbers.
+    '''
+    trueCounter = 0
+    total = 0
+    for i in range(len(predictions)):
+        if is_correct(predictions[i],labels[i]):
+            trueCounter += 1
+        total += 1
+        
+    return trueCounter/total *100
+
+print(percent_correct(predictions, labels))
---
+++ finals/final_2019.py
@@ -0,0 +1,346 @@
+# print((2 < 1 and "g" > "a") or not 'a' == 'b')
+# print(list(range(4,8,2)))
+
+# f = open('f.txt', 'w')
+# f.write('Hello\n')
+# f.write('\n')
+# f.close()
+# f = open('f.txt', 'w')
+# f.write('Fun!')
+# f.close()
+
+# def fun1(num):
+#     if num < 0:
+#         return False
+#     elif num > 0:
+#         return True
+#     print(num ** 0.5)
+# print(fun1(4))
+
+# class Date:
+#     def __init__(self, year, month, day):
+#         self.year = year
+#         self.month = month
+#         self.day = day 
+# class Dog:
+#     def __init__(self, first_name, adoption_date):
+#         self.first_name = first_name
+#         self.adoption_date = adoption_date
+# adoption_date = Date(2019, 3, 28)
+# my_dog = Dog("Barchimedes", adoption_date)
+# adoption_date = Date(2017, 1, 1) 
+# adoption_date.year = 2018
+# print(my_dog.adoption_date.year)
+
+
+# '''
+# 1 1  1
+# 1 2  2
+# 2 2  4
+# '''
+
+# def my_func(x,y):
+#     print(x,y,end = " ")
+#     return x*y
+# max_num = 3
+# for i in range(1,max_num):
+#     for j in range(i,max_num):
+#         print(my_func(i,j))
+
+'''
+416-1212
+True
+None
+'''
+
+# phonebook = {'Akako':'416-1212',
+#  'Ke':'647-2122',
+#  'Alya':'416-9923'}
+
+# print(phonebook['Akako'])
+# print("Ke" in phonebook)
+# print(phonebook["Freya"])
+
+'''
+1
+3
+5
+'''
+
+# class Node:
+#     def __init__(self, cargo = 0, next = None):
+#         self.cargo = cargo
+#         self.next = next
+# def print_special(n, p):
+#     if n is None:
+#         return
+#     elif p:
+#         print(n.cargo)
+ 
+#     print_special(n.next, not p)
+# n = Node(1, Node(2, Node(3, Node(4, Node(5)))))
+# print_special(n, True)
+
+def stopping_criteria(num):
+    if num == 6:
+        return True
+    return False
+
+temps = [1,2,3,4,5,6,7,8]
+selected_temps = []
+
+def extract_nums(temps, selected_temps):
+    '''([num,num,…], []) -> NoneType'''
+    index = 0
+    while not stopping_criteria(temps[index]):
+        selected_temps.append(temps[index])
+        index += 1
+    
+# extract_nums(temps,selected_temps)
+# print(selected_temps)
+
+# temps = [1,2,3,4,5,6,7,8]
+# selected_temps = []    
+
+# def extract_nums2(temps, selected_temps):
+#     for num in temps:
+#         if not stopping_criteria(num):
+#             selected_temps.append(num)
+#         else:
+#             return
+
+# extract_nums2(temps,selected_temps)
+# print(selected_temps)
+
+
+class NodeBT:
+    def __init__(self, c, l = None, r = None):
+        '''Creates an object of type NodeBT whose cargo value is c. 
+        '''
+        self.cargo = c
+        self.left = l
+        self.right = r
+
+    def sum_all_nodes(self):
+        ''' (NodeBT) -> num
+        Returns the sum of the cargo values of all the nodes in the binary 
+        tree that is rooted at self.
+        Precondition: All nodes have cargo values that are integers.'''    
+        final = 0
+        level = [self]
+        
+        while len(level) > 0:
+            level_next = []
+            
+            for node in level:
+                final += node.cargo
+                if node.left is not None:
+                    level_next.append(node.left)
+                if node.right is not None:
+                    level_next.append(node.right)
+            
+            level = level_next
+        
+        return final
+    
+    def print_tree(self):
+        level = [self]
+        
+        while len(level) > 0:
+            
+            level_next = []
+            
+            for node in level:
+                
+                print(node.cargo, " ", end = "")
+                
+                if node.left is not None:
+                    level_next.append(node.left) 
+                
+                if node.right is not None:
+                    level_next.append(node.right)
+                
+                    
+            print('\n')
+            level = level_next
+                
+
+# t = NodeBT(1, NodeBT(2), NodeBT(3, NodeBT(4)))
+# print(t.sum_all_nodes())
+
+#t.print_tree()
+
+
+class BinaryTree:
+     
+    def __init__(self, tree_root):
+        ''' Constructs a binary tree with the NodeBT tree_root 
+        as its root.'''
+        
+        self.root = tree_root
+    
+        self.tree_sum = self.root.sum_all_nodes()
+
+
+
+def tree_merge(new_root, t1, t2):
+    '''(NodeBT, BinaryTree, BinaryTree)-> BinaryTree 
+    Returns a binary tree whose root is new_root and whose 
+    left and right subtrees contain the nodes of t1 and t2, 
+    respectively.'''
+    new_root.left = t1.root
+    new_root.right = t2.root
+    new_tree = BinaryTree(new_root)
+    return new_tree
+
+# r1 = NodeBT(2, NodeBT(1), NodeBT(3))
+# t1 = BinaryTree(r1)
+# r2 = NodeBT(6, NodeBT(5), NodeBT(8))
+# t2 = BinaryTree(r2)
+# print("ssssss")
+# t1.root.print_tree()
+# print("ssssss")
+# t2.root.print_tree()
+
+
+# r = NodeBT(4)
+# t = tree_merge(r, t1, t2)
+
+# print("root cargo = ", t.root.cargo)
+# print("tree total = ", t.tree_sum)
+
+
+    
+class Date:
+    '''
+    Stores and manipulates dates from January 1, 1900
+    '''
+    def __init__(self,year=1900,month=1,day=1):
+        self.year = year
+        self.day = day
+        self.month = month
+    
+    def __str__(self):
+        months = ['', 'January', 'February', 'March', 
+                  'April', 'May', 'June', 'July', 
+                  'August', 'September', 'October', 'November', 'December']
+        return months[self.month] + " "+ str(self.day) +", " + str(self.year)
+
+    def valid_date(self):
+        '''
+        Returns True if the object contains a valid date in 1900 or later.
+        Otherwise, returns False.
+        Does not deal with leap years: assumes that February has 28 days 
+        '''
+        if self.year < 1900:
+            return False
+        
+        if self.month < 1 or self.month > 12:
+            return False
+        
+        if self.day <= 0:
+            return False
+        
+        if (self.month in [1,3,5,7,8,10,12]) and self.day > 31:
+            return False
+        
+        if (self.month in [4,6,9,11]) and self.day > 30:
+            return False
+        
+        if (self.month == 2) and self.day > 28:
+            return False
+        
+        return True
+        
+        
+        
+        
+#print(Date(2019,2,30).valid_date())
+
+
+def scramble_items(sample_list):
+    '''
+    [item, item,… ,item] -> [str, str,… ,str]
+    Input: list of items that could be int, float, or string.
+    Output: list of scrambled strings.
+    '''
+    returnList = []
+    for item in sample_list:
+        item = str(item)
+        scrabledList = []
+        
+        for even in range(1,len(item),2):
+            scrabledList.append(item[even])
+        for odd in range(0,len(item),2):
+            scrabledList.append(item[odd])
+        
+        combinedWords =""
+        
+        for i in scrabledList:
+            combinedWords += (i)
+            
+        returnList.append(combinedWords)
+    return returnList
+
+sample_list = ['Elon Tusk', 420, 'Mars Rd.', 343521]
+new_list = scramble_items(sample_list)
+#print(new_list)
+
+import csv
+def write_scrambled_items(filename, original_data):
+    '''
+    (str, [[item, item,… ,item], …]) -> NoneType
+    Input: a string and a nested list with list items being any 
+    combination of int, float and string types.
+    Output: nothing is returned. A scrambled version of the nested list 
+    data is saved to filename using comma separated value format.
+    '''
+    scrambledList = []
+    
+    for i in original_data:
+        scrambledList.append(scramble_items(i))
+    
+    with open(filename, "w") as file:
+        writer = csv.writer(file)
+        for i in scrambledList:
+            writer.writerow(i)
+
+# data = [['Elon Tusk', 420, 'Mars Rd.', 343521],
+#  ['Haramble', 123, 'Zoo Ave.', 123456],
+#  ['Snoop Dug', 42, 'Boring St.', 4578091]]
+# write_scrambled_items('data.csv', data)
+
+def calc_averages(lab_grades):
+    '''
+    [[str,…,str],[num,…,num], …] -> [[str, str],[num, num], …]
+
+    Input: a nested list of strings and integers representing field names, student 
+    ids and lab grades which may have ‘e’ included.
+    Output: nested list containing the student ID and average grade on the labs 
+    calcula
+    '''
+    returnLst = [['Student ID', 'Average Grade'],]
+    for person in range(1,len(lab_grades)):
+        studentId = lab_grades[person][0]
+
+        totalGradeCounter = []
+        for grades in lab_grades[person]:
+            if grades != studentId and grades != "e":
+           
+                totalGradeCounter.append(grades)
+        lowest = totalGradeCounter[0]
+        for i in totalGradeCounter:
+            if i < lowest:
+                lowest = i
+        totalGradeCounter.remove(lowest)
+        print(totalGradeCounter)
+
+        
+        returnLst.append([studentId,sum(totalGradeCounter)/len(totalGradeCounter)])
+                
+    return returnLst
+lab_grades = [['ID','Lab 1','Lab 2','Lab 3','Lab 4','Lab 5','Lab 6', 'Lab 7','Lab 8','Lab 9'], [1001, 5, 8, 3, 2, 
+3, 6, 1, 0, 9],[1002, 9, 'e', 10, 9, 9, 9, 'e', 9, 10], [1003, 9, 
+7, 9, 'e', 5, 9, 8, 6, 8]]
+avg_grades = calc_averages(lab_grades)
+print(avg_grades)
